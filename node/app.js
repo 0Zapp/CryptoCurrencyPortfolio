b435a765c6e0f80dc1e25d08ba4f65dbc021119a
Marko Nedeljkovic <marko65734@gmail.com>
2021-02-14 22:34:22 +0100

    preparation and commit testing, gitignore

--- node/app.js
+++ node/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const msgs = require('./routes/messages');  // Nas ruter (REST API)
+const history = require('connect-history-api-fallback');
+const path = require('path');
 
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 // Kazemo aplikaciji da za rute koje pocinju sa '/api' koristi nas ruter
 app.use('/api', msgs);
 
-app.get('/', (req, res) => {
-    res.send('Zdravo!! :)');
-});
+const staticMiddleware = express.static(path.join(__dirname, 'dist'));
+
+app.use(staticMiddleware);
+app.use(history());
+app.use(staticMiddleware);
 
 app.listen(80);
