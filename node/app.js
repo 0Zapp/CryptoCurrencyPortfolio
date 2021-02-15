1d2a4456edf2896ffee3aa61098f1230bdefde3e
Marko Nedeljkovic <marko65734@gmail.com>
2021-02-15 10:19:29 +0100

    small changes, checkpoint for big changes

--- node/app.js
+++ node/app.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
+    //res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
