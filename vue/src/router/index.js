0d33e7105bfeadd54f79d77240bc2b4f8a95d8ce
Marko Nedeljkovic <marko65734@gmail.com>
2021-02-15 18:52:27 +0100

    authentication on frontend

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Message from "@/views/Message";
 import NewMessage from "@/views/NewMessage";
+import Callback from "@/components/Callback.vue";
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/message/:id',
     name: 'message',
     component: Message
+  },
+  {
+    path: '/callback',
+    name: 'Callback',
+    component: Callback
   }
 ]
 
